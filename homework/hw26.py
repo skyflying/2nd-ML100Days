af3b3ae7c0a3644003c829c96d3a8b1712d57dd5
Ming-Yi Hsu <skyflying07@gmail.com>
2019-05-13 23:02:06 +0800

    Day26_HW

---
+++ homework/hw26.py
@@ -0,0 +1,84 @@
+# 做完特徵工程前的所有準備
+import pandas as pd
+import numpy as np
+import datetime
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
+import warnings
+warnings.filterwarnings('ignore')
+
+
+data_path ='E:\\python\\ML-day100\\data\\'
+df = pd.read_csv(data_path + 'taxi_data1.csv')
+
+train_Y = df['fare_amount']
+df = df.drop(['fare_amount'] , axis=1)
+df.head()
+
+
+
+
+
+# 時間特徵分解方式:使用datetime
+df['pickup_datetime'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S UTC'))
+df['pickup_year'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strftime(x, '%Y')).astype('int64')
+df['pickup_month'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strftime(x, '%m')).astype('int64')
+df['pickup_day'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strftime(x, '%d')).astype('int64')
+df['pickup_hour'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strftime(x, '%H')).astype('int64')
+df['pickup_minute'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strftime(x, '%M')).astype('int64')
+df['pickup_second'] = df['pickup_datetime'].apply(lambda x: datetime.datetime.strftime(x, '%S')).astype('int64')
+df.head()
+
+
+
+# 將結果使用線性迴歸 / 梯度提升樹分別看結果
+df = df.drop(['pickup_datetime'] , axis=1)
+scaler = MinMaxScaler()
+train_X = scaler.fit_transform(df)
+Linear = LinearRegression()
+print(f'Linear Reg Score : {cross_val_score(Linear, train_X, train_Y, cv=5).mean()}')
+GDBT = GradientBoostingRegressor()
+print(f'Gradient Boosting Reg Score : {cross_val_score(GDBT, train_X, train_Y, cv=5).mean()}')
+
+
+
+
+
+# 增加緯度差, 經度差, 座標距離等三個特徵
+df['longitude_diff'] = df['dropoff_longitude'] - df['pickup_longitude']
+df['latitude_diff'] = df['dropoff_latitude'] - df['pickup_latitude']
+df['distance_2D'] = (df['longitude_diff']**2 + df['latitude_diff']**2)**0.5
+df[['distance_2D', 'longitude_diff', 'latitude_diff', 'pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude']].head()
+
+
+
+
+
+# 結果 : 準確度上升
+train_X = scaler.fit_transform(df)
+print(f'Linear Reg Score : {cross_val_score(Linear, train_X, train_Y, cv=5).mean()}')
+print(f'Gradient Boosting Reg Score : {cross_val_score(GDBT, train_X, train_Y, cv=5).mean()}')
+
+
+
+
+
+
+import math
+
+df['distance_real'] = ((df['longitude_diff']/0.75756)**2 + df['latitude_diff']**2)**0.5
+
+# 觀察結果 
+train_X = scaler.fit_transform(df)
+print(f'Linear Reg Score : {cross_val_score(Linear, train_X, train_Y, cv=5).mean()}')
+print(f'Gradient Boosting Reg Score : {cross_val_score(GDBT, train_X, train_Y, cv=5).mean()}')
+
+
+
+
+
+train_X = scaler.fit_transform(df[['distance_real']])
+print(f'Linear Reg Score : {cross_val_score(Linear, train_X, train_Y, cv=5).mean()}')
+print(f'Gradient Boosting Reg Score : {cross_val_score(GDBT, train_X, train_Y, cv=5).mean()}')
