9ca7a783b74d4efab3858ed43d2725e3fe708e20
Ming-Yi Hsu <skyflying07@gmail.com>
2019-05-16 15:51:17 +0800

    hw_27 update

--- homework/hw27.py
+++ homework/hw27.py
@@ -5,8 +5,9 @@ import copy
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
-
-data_path = 'data/'
+import warnings
+warnings.filterwarnings('ignore')
+data_path = '~/Downloads/'
 df = pd.read_csv(data_path + 'titanic_train.csv')
 
 train_Y = df['Survived']
@@ -18,15 +19,18 @@ df.head()
 
 # 取一個類別型欄位, 與一個數值型欄位, 做群聚編碼
 
+#取cabin欄位，做平均值眾數以及中位數等數值欄位做群聚編碼
 df['Cabin'] = df['Cabin'].fillna('None')
-mean_df = df.groupby(['Cabin'])['Fare'].mean().reset_index()
-mode_df = df.groupby(['Cabin'])['Fare'].apply(lambda x: x.mode()[0]).reset_index()
-median_df = df.groupby(['Cabin'])['Fare'].median().reset_index()
-max_df = df.groupby(['Cabin'])['Fare'].max().reset_index()
-temp = pd.merge(mean_df, mode_df, how='left', on=['Cabin'])
-temp = pd.merge(temp, median_df, how='left', on=['Cabin'])
-temp = pd.merge(temp, max_df, how='left', on=['Cabin'])
-temp.columns = ['Cabin', 'Cabin_Fare_Mean', 'Cabin_Fare_Mode', 'Cabin_Fare__Median', 'Cabin_Fare_Max']
+df_mean = df.groupby(['Cabin'])['Fare'].mean().reset_index()
+df_mode = df.groupby(['Cabin'])['Fare'].apply(lambda x: x.mode()[0]).reset_index()
+df_median = df.groupby(['Cabin'])['Fare'].median().reset_index()
+df_max= df.groupby(['Cabin'])['Fare'].max().reset_index()
+df_min= df.groupby(['Cabin'])['Fare'].min().reset_index()
+temp = pd.merge(df_mean, df_mode, how='left', on=['Cabin'])
+temp = pd.merge(temp, df_median, how='left', on=['Cabin'])
+temp = pd.merge(temp, df_max, how='left', on=['Cabin'])
+temp = pd.merge(temp, df_min, how='left', on=['Cabin'])
+temp.columns = ['Cabin', 'Cabin_Fare_Mean', 'Cabin_Fare_Mode', 'Cabin_Fare__Median', 'Cabin_Fare_Max', 'Cabin_Fare_Min']
 temp
 
 
@@ -60,11 +64,11 @@ df.head()
 
 # 原始特徵 + 邏輯斯迴歸
 
-# 沒有這四個新特徵的 dataframe 稱為 df_minus
-df_minus = df.drop(['Cabin_Fare_Mean', 'Cabin_Fare_Mode', 'Cabin_Fare__Median', 'Cabin_Fare_Max'] , axis=1)
+# 沒有這四個新特徵的 dataframe 稱為 df_temp
+df_temp = df.drop(['Cabin_Fare_Mean', 'Cabin_Fare_Mode', 'Cabin_Fare__Median', 'Cabin_Fare_Max', 'Cabin_Fare_Min'] , axis=1)
 
 # 原始特徵 + 線性迴歸
-train_X = MMEncoder.fit_transform(df_minus)
+train_X = MMEncoder.fit_transform(df_temp)
 estimator = LogisticRegression()
 cross_val_score(estimator, train_X, train_Y, cv=5).mean()
 
