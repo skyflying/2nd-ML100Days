cddac10aecb799943c68dbc1e0a71ab83132bfc8
Ming-Yi Hsu <skyflying07@gmail.com>
2019-06-22 10:23:44 +0800

    Day57

---
+++ homework/hw57.py
@@ -0,0 +1,70 @@
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import AgglomerativeClustering
+
+
+
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+np.random.seed(5)
+
+%matplotlib inline
+
+
+
+estimators = [('hc_ward', AgglomerativeClustering(n_clusters=3, linkage="ward")),
+              ('hc_complete', AgglomerativeClustering(n_clusters=3, linkage="complete")),
+              ('hc_average', AgglomerativeClustering(n_clusters=3, linkage="average"))]
+			  
+			  
+			  
+			  
+
+fignum = 1
+titles = ['ward', 'complate', 'average']
+for name, est in estimators:
+    fig = plt.figure(fignum, figsize=(4, 3))
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+    
+    ## fit data
+    est.fit(X)
+    
+    labels = est.labels_
+
+    ax.scatter(X[:, 3], X[:, 0], X[:, 2],
+               c=labels.astype(np.float), edgecolor='k')
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    ax.set_title(titles[fignum - 1])
+    ax.dist = 12
+    fignum = fignum + 1
+
+
+fig = plt.figure(fignum, figsize=(4, 3))
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+for name, label in [('cls0', 0),
+                    ('cls1', 1),
+                    ('cls2', 2)]:
+    ax.text3D(X[y == label, 3].mean(),
+              X[y == label, 0].mean(),
+              X[y == label, 2].mean() + 2, name,
+              horizontalalignment='center',
+              bbox=dict(alpha=.2, edgecolor='w', facecolor='w'))
+
+y = np.choose(y, [1, 2, 0]).astype(np.float)
+ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor='k')
+
+ax.w_xaxis.set_ticklabels([])
+ax.w_yaxis.set_ticklabels([])
+ax.w_zaxis.set_ticklabels([])
+ax.set_title('Ground Truth')
+ax.dist = 12
+
+
