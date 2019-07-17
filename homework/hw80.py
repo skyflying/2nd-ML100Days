ba2b326e0337e8f2848ce5b505557fbecdbff833
Ming-Yi Hsu <skyflying07@gmail.com>
2019-07-17 22:34:33 +0800

    Day80 revise
    add SGD

--- homework/hw80.py
+++ homework/hw80.py
@@ -223,3 +223,51 @@ for i, cond in enumerate(results.keys()):
 plt.title("Accuracy")
 plt.legend()
 plt.show()
+
+
+
+
+results = {}
+
+
+for lr in LEARNING_RATE:
+    keras.backend.clear_session() # §âÂÂªº Graph ²M±¼
+    print("Experiment with LR = %.6f" % (lr))
+    model = build_mlp(input_shape=x_train.shape[1:])
+    model.summary()
+    optimizer = keras.optimizers.SGD(lr=lr, nesterov=True, momentum=MOMENTUM)
+    model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
+
+    model.fit(x_train, y_train, 
+              epochs=EPOCHS, 
+              batch_size=BATCH_SIZE, 
+              validation_data=(x_test, y_test), 
+              shuffle=True)
+    
+    # Collect results
+    train_loss = model.history.history["loss"]
+    valid_loss = model.history.history["val_loss"]
+    train_acc = model.history.history["acc"]
+    valid_acc = model.history.history["val_acc"]
+    
+    exp_name_tag = "SGD exp-lr-%s" % str(lr)
+    results[exp_name_tag] = {'train-loss': train_loss,
+                             'valid-loss': valid_loss,
+                             'train-acc': train_acc,
+                             'valid-acc': valid_acc}
+							 
+							 
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-loss'])),results[cond]['train-loss'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-loss'])),results[cond]['valid-loss'], '--', label=cond, color=color_bar[i])
+plt.title("Loss")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-acc'])),results[cond]['train-acc'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-acc'])),results[cond]['valid-acc'], '--', label=cond, color=color_bar[i])
+plt.title("Accuracy")
+plt.legend()
+plt.show(
